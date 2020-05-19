e29fd26271c553effd9a2a99a59c772ea8186c36
TruongVyPhat <63778351+TruongVyPhat@users.noreply.github.com>
2020-05-20 01:47:57 +0700

    commit userAPI + authentication

---
+++ app/api/functions/format_time.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// chuyển kết quả từ gettime() sang format time
+exports.gettime_to_format = function(time){
+    let d = new Date(time);
+    let year = d.getFullYear();
+    let month = d.getMonth() + 1;
+    let day =  d.getDate();
+    let hour = d.getHours();
+    let min = d.getMinutes();
+    let sec = d.getSeconds();
+
+    if (day < 10){
+        day = '0'+ day;
+    }
+    if (month < 10){
+        month = '0' + month;
+    }
+    if (hour < 10){
+        hour = '0' + hour;
+    }
+    if (min < 10){
+        min = '0' + min; 
+    }
+    if (sec < 10){
+        sec = '0' + sec;
+    }
+    return year+'-'+month+'-'+day+' '+hour+':'+min+':'+sec;
+};
