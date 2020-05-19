f65c1fa760e5b19f040c1ece874f93224f40516a
1653020 <46862344+1653020@users.noreply.github.com>
2020-05-19 23:11:36 +0700

    SignUp Validation

--- client/src/views/auth/SignUp.js
+++ client/src/views/auth/SignUp.js
@@ -3,8 +3,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -12,6 +10,9 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { Link } from 'react-router-dom';
+import { string as yupstring, object as yupobject } from "yup";
+import * as Yup from 'yup'
+import { useForm } from "react-hook-form";
 
 function Copyright() {
     return (
@@ -46,12 +47,35 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+const LoginSchema = yupobject().shape({
+    firstName: yupstring()
+        .required()
+        .matches(/[a-zA-Z]/),
+    lastName: yupstring()
+        .required()
+        .matches(/[a-zA-Z]/),
+    email: yupstring()
+        .required()
+        .email(),
+    password: yupstring()
+        .required()
+        .min(8)
+        .matches(/[a-zA-Z]/),
+    passwordconfirm: yupstring()
+    .oneOf([Yup.ref('password'), null])
+    
+});
+
 function SignUp() {
     const classes = useStyles();
 
-    const handleSubmit = e => {
-        console.log("submit")
-    }
+    const {register, handleSubmit, errors } = useForm({
+        validationSchema: LoginSchema
+    });
+
+    const onSubmit = (data) => {
+        console.log("data", data);
+    };
 
     return (
         <Container component="main" maxWidth="xs">
@@ -63,21 +87,21 @@ function SignUp() {
                 <Typography component="h1" variant="h5">
                     Sign up
                 </Typography>
-                <form onSubmit={(e) => {
-                        e.preventDefault();
-                        handleSubmit();
-                }} className={classes.form} noValidate>
+                <br/>
+                <form onSubmit={handleSubmit(onSubmit)} noValidate>
                     <Grid container spacing={2}>
                         <Grid item xs={12} sm={6}>
                             <TextField
                                 autoComplete="fname"
                                 name="firstName"
                                 variant="outlined"
-                                required
                                 fullWidth
                                 id="firstName"
                                 label="First Name"
                                 autoFocus
+                                inputRef={register}
+                                error={errors.firstName ? true : false}
+                                helperText={errors.firstName ? "First Name required." : ""}
                             />
                         </Grid>
                         <Grid item xs={12} sm={6}>
@@ -89,6 +113,9 @@ function SignUp() {
                                 label="Last Name"
                                 name="lastName"
                                 autoComplete="lname"
+                                inputRef={register}
+                                error={errors.lastName ? true : false}
+                                helperText={errors.lastName ? "Last Name required." : ""}
                             />
                         </Grid>
                         <Grid item xs={12}>
@@ -100,6 +127,9 @@ function SignUp() {
                                 label="Email Address"
                                 name="email"
                                 autoComplete="email"
+                                inputRef={register}
+                                error={errors.email ? true : false}
+                                helperText={errors.email ? "Email incorrect." : ""}
                             />
                         </Grid>
                         <Grid item xs={12}>
@@ -112,12 +142,24 @@ function SignUp() {
                                 type="password"
                                 id="password"
                                 autoComplete="current-password"
+                                inputRef={register}
+                                error={errors.password ? true : false}
+                                helperText={errors.password ? "Password is at least 8 characters and there are no special characters" : ""}
                             />
                         </Grid>
                         <Grid item xs={12}>
-                            <FormControlLabel
-                                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                label="I want to receive inspiration, marketing promotions and updates via email."
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="passwordconfirm"
+                                label="Password Confirm"
+                                type="password"
+                                id="passwordconfirm"
+                                autoComplete="current-password"
+                                inputRef={register}
+                                error={errors.passwordconfirm ? true : false}
+                                helperText={errors.passwordconfirm ? "Password not match." : ""}
                             />
                         </Grid>
                     </Grid>
