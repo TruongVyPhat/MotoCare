1bc0dd0c7ef8ecbad3bae1a4f50661522d4019e3
TruongVyPhat <63778351+TruongVyPhat@users.noreply.github.com>
2020-05-25 18:30:58 +0700

    change responseJS + add user-permisson

--- app/api/helpers/json-generator.js
+++ app/api/helpers/json-generator.js
@@ -1,4 +1,15 @@
-exports.Json = {
-    message : '',
-    data: []    
+const httpStatus = require('http-status-codes');
+
+exports.mess_Json = (status) => {
+    return {
+        message : httpStatus.getStatusText(status),
+        data: []
+    }    
 };
+
+exports.Json = (status, data) => {
+    return {
+        message: httpStatus.getStatusText(status),
+        data: data
+    }
+}
--- app/api/middlewares/permission.js
+++ app/api/middlewares/permission.js
@@ -1,8 +1,6 @@
-const HttpStatus = require('http-status-codes');
-let responseJS = require('../helpers/json-generator').Json; 
-const db = require('../../models/index');
-const sequelize = db.sequelize;
-const { QueryTypes } = require('sequelize');
+const httpStatus = require('http-status-codes');
+const responseJS = require('../helpers/json-generator'); 
+//const report_service = require('../reports/ReportServices');
 
 // CHECK user permission
 exports.user_permission = (allowed) => {
@@ -12,35 +10,38 @@ exports.user_permission = (allowed) => {
         if (request.user && isAllowed(request.user.role_id)){
             next();
         }else {
-            status = HttpStatus.FORBIDDEN;
-            responseJS.message = HttpStatus.getStatusText(status);
-            response.status(status).json(responseJS);
+            status = httpStatus.FORBIDDEN;
+            response.status(status).json(responseJS.mess_Json(status));
         }   
     }
 };
 
-//  CHECK user permission for report and rpDetail
-//  rp_detail_id != null => delete, update reportDetail 
-//  rp_detail_id == null => use for REPORT and create reportDETAIL
-exports.report_permission = (report_id, user_id, rp_detail_id) => {
-    let permission = true;
-    let query = 'SELECT created_by from Report where id = ?';
-    if (rp_detail_id){
-        query = 'SELECT rp.created_by from Report rp JOIN ReportDetail rd on rp.id = rd.report_id where rd.id = ?';
-    }
-    sequelize.query(query, {
-        replacements: rp_detail_id? [rp_detail_id]: [report_id],
-        type: QueryTypes.SELECT
-    }).then(function(report){
-        if (!report[0] || report[0].created_by !== user_id){
-            permission = false;
-            status = HttpStatus.FORBIDDEN;
-            responseJS.message = HttpStatus.getStatusText(status);
-            res.status(status).json(responseJS);
-        }
-    }).catch(function(error) {
-        permission = false;
-        res.status(status).json(error);
-    });        
-    return permission;
-};
+/**
+ *  CHECK user permission for report and rpDetail
+ * rp_detail_id != null => delete, update reportDetail 
+ * rp_detail_id == null => use for REPORT and create reportDETAIL
+ */
+
+// exports.report_permission = (report_id, user_id, rp_detail_id) => {
+    
+//     return (request, response, next) => {
+//         report_service.get_created_by(report_id, rp_detail_id)
+//         .then(report => {
+//             if (report[0]){
+//                 if (report[0].created_by === user_id){
+//                     next();
+//                 } else {
+//                     const status = httpStatus.FORBIDDEN;
+//                     res.status(status).json(responseJS.mess_Json(status));
+//                 }
+                
+//             } else {
+//                 const status = httpStatus.NOT_FOUND;
+//                 res.status(status).json(responseJS.mess_Json(status));
+//             }
+//         }).catch(function(error) {
+//             res.status(status).json(error);
+//             return;
+//         });
+//     };        
+// };
