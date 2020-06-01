cb3235ec5bf3e156404fd8207fe1cf4eea2c7676
TruongVyPhat <63778351+TruongVyPhat@users.noreply.github.com>
2020-06-01 18:52:43 +0700

    commit service-api

---
+++ app/api/services/ServiceControllers.js
@@ -0,0 +1,83 @@
+const service = require('./ServiceServices');
+const httpStatus = require('http-status-codes');
+const CONSTANTS = require('../helpers/constants');
+const ROLE = CONSTANTS.ROLE;
+const formatter_time = require('../functions/format_time');
+const responseJS = require('../helpers/json-generator');
+let status = httpStatus.INTERNAL_SERVER_ERROR;
+
+exports.get_all_services = (req, res) => {
+    const page = req.query.page;
+    service.get_all_services(page)
+    .then(services => {
+        if (services){
+            status = httpStatus.OK; 
+            res.status(status).json(responseJS.Json(status, services));
+        } else {
+            status = httpStatus.NOT_FOUND;
+            res.status(status).json(responseJS.mess_Json(status));
+        }
+    })
+    .catch(function(error) {
+        res.status(status).json(error);
+    });
+}
+
+exports.get_service = (req, res) => {
+    const service_id = req.params.id;
+    service.get_service(service_id)
+    .then(services => {
+        if (services){
+            status = httpStatus.OK;
+            res.status(status).json(responseJS.Json(status, services[0]));
+        } else {
+            status = httpStatus.NOT_FOUND;
+            res.status(status).json(responseJS.mess_Json(status));
+        }
+    }).catch(error => {
+        res.status(status).json(error);
+    });
+}
+
+exports.create_service = (req, res) => {
+    const name = req.body.data.name.trim();
+    const duration = req.body.data.duration;
+    const price = req.body.data.price;
+    const image = req.body.data.image;
+    
+    service.create_service(name, duration, price, image)
+    .then(created => {
+        status = httpStatus.OK;
+        res.status(status).json(responseJS.mess_Json(status));
+    }).catch(function(error) {
+        res.status(status).json(error);
+    });
+}
+
+exports.update_service = (req, res) => {
+    const id = req.params.id
+    const name = req.body.data.name.trim();
+    const duration = req.body.data.duration;
+    const price = req.body.data.price;
+    const image = req.body.data.image;
+    
+    service.update_service(name, duration, price, image, id)
+    .then(updating => {
+        status = httpStatus.OK;
+        res.status(status).json(responseJS.mess_Json(status));
+    }).catch(function(error) {
+        res.status(status).json(error);
+    });
+};
+
+exports.delete_service = (req, res) => {
+    const id = req.params.id;
+    
+    service.delete_service(id)
+    .then(deleted => {
+        status = httpStatus.OK;
+        res.status(status).json(responseJS.mess_Json(status));
+    }).catch(function(error) {
+        res.status(status).json(error);
+    });
+}
