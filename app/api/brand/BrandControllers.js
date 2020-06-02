d38e913dbc45601c7507d1a34e6e4621d4fdcfd7
TruongVyPhat <63778351+TruongVyPhat@users.noreply.github.com>
2020-06-02 14:39:03 +0700

    commit brand-api

---
+++ app/api/brand/BrandControllers.js
@@ -0,0 +1,74 @@
+const service = require('./BrandServices');
+const httpStatus = require('http-status-codes');
+const responseJS = require('../helpers/json-generator');
+let status = httpStatus.INTERNAL_SERVER_ERROR;
+
+exports.get_all_brands = (req, res) => {
+    const page = req.query.page;
+    service.get_all_brands(page)
+    .then(brands => {
+        if (brands){
+            status = httpStatus.OK; 
+            res.status(status).json(responseJS.Json(status, brands));
+        } else {
+            status = httpStatus.NOT_FOUND;
+            res.status(status).json(responseJS.mess_Json(status));
+        }
+    })
+    .catch(function(error) {
+        res.status(status).json(error);
+    });
+}
+
+exports.get_brand = (req, res) => {
+    const brand_id = req.params.id;
+    service.get_brand(brand_id)
+    .then(brands => {
+        if (brands){
+            status = httpStatus.OK;
+            res.status(status).json(responseJS.Json(status, brands[0]));
+        } else {
+            status = httpStatus.NOT_FOUND;
+            res.status(status).json(responseJS.mess_Json(status));
+        }
+    }).catch(error => {
+        res.status(status).json(error);
+    });
+}
+
+exports.create_brand = (req, res) => {
+    const name = req.body.data.name.trim();
+    
+    service.create_brand(name)
+    .then(created => {
+        status = httpStatus.OK;
+        res.status(status).json(responseJS.mess_Json(status));
+    }).catch(function(error) {
+        res.status(status).json(error);
+    });
+}
+
+exports.update_brand = (req, res) => {
+    const id = req.params.id
+    const name = req.body.data.name.trim();
+    
+    service.update_brand(name, id)
+    .then(updating => {
+        status = httpStatus.OK;
+        res.status(status).json(responseJS.mess_Json(status));
+    }).catch(function(error) {
+        res.status(status).json(error);
+    });
+}
+
+exports.delete_brand = (req, res) => {
+    const id = req.params.id;
+    
+    service.delete_brand(id)
+    .then(deleted => {
+        status = httpStatus.OK;
+        res.status(status).json(responseJS.mess_Json(status));
+    }).catch(function(error) {
+        res.status(status).json(error);
+    });
+}
---
+++ app/api/brand/BrandRouters.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const brand_Controller = require('./BrandControllers');
+const auth_Controller = require('../auth/AuthControllers');
+const ROLE = require('../helpers/constants').ROLE;
+const permission = require('../middlewares/permission');
+
+/**
+ * GET list all brands
+ * URL: /api/brands?page=
+ */
+router.get('/', brand_Controller.get_all_brands);
+
+// GET brand
+router.get('/:id', brand_Controller.get_brand);
+
+// CREATE brand
+router.post('/', /*auth_Controller.isAuthenticated, permission.user_permission([ROLE.ADMIN]),*/ brand_Controller.create_brand);
+
+// UPDATE brand
+router.put('/:id', /*auth_Controller.isAuthenticated, permission.user_permission([ROLE.ADMIN, ROLE.STAFF]),*/ brand_Controller.update_brand);
+
+// DELETE brand
+router.delete('/:id', /*auth_Controller.isAuthenticated, permission.user_permission([ROLE.ADMIN]),*/ brand_Controller.delete_brand);
+
+module.exports = router;
