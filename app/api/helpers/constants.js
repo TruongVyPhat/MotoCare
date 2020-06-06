d42c36239bac1bd601688ddc6d53d19d6b66f725
TruongVyPhat <63778351+TruongVyPhat@users.noreply.github.com>
2020-06-06 14:53:49 +0700

    add constants

--- app/api/helpers/constants.js
+++ app/api/helpers/constants.js
@@ -5,5 +5,17 @@ module.exports = {
         ADMIN: 1,
         STAFF: 2,
         CUSTOMER: 3
-    }
+    },
+    BOOKING_STATUS: {
+        booked: 'booked',
+        processing: 'processing',
+        done: 'done',
+        cancle: 'cancle',
+        not_come: 'not-come',
+    },
+    milisec_per_day: 86400000,
+    milisec_per_hour: 3600000,
+    max_day: 2,
+    min_hour: 1,
+    DEFAULT_DISCOUNT: 0
 };
