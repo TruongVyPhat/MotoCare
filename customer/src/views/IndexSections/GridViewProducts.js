bf8acdae58a0d930d0b83860f6bd7fac4882245a
TruongVyPhat <63778351+TruongVyPhat@users.noreply.github.com>
2020-06-08 19:50:14 +0700

    commit filter category

--- customer/src/views/IndexSections/GridViewProducts.js
+++ customer/src/views/IndexSections/GridViewProducts.js
@@ -3,18 +3,28 @@ import { Card } from 'semantic-ui-react';
 import Product from './Product';
 import axios from 'axios';
 
-const GridViewProducts = ({search}) => {
+const GridViewProducts = ({search, category}) => {
     
     const [listProduct, setListProduct] = useState([]);
     useEffect(() => {
         if (!search){
-            axios.get(`http://localhost:9000/api/products?page=1`)
-            .then(res => {
-                setListProduct(res.data.data);
-            }).catch(error => {
-                console.log(error);
-            });
-        } else {
+            if(category){
+                axios.get(`http://localhost:9000/api/products?category=${category}&page=1`)
+                .then(res => {
+                    setListProduct(res.data.data);
+                }).catch(error => {
+                    console.log(error);
+                });
+            }else {
+                axios.get(`http://localhost:9000/api/products?page=1`)
+                .then(res => {
+                    setListProduct(res.data.data);
+                }).catch(error => {
+                    console.log(error);
+                });
+            }
+        } 
+        else {
             axios.get(`http://localhost:9000/api/products/search?keyword=${search}`)
             .then(res => {
                 setListProduct(res.data.data);
@@ -22,7 +32,7 @@ const GridViewProducts = ({search}) => {
                 console.log(error);
             });
         }
-    }, [search]);
+    }, [search, category]);
     
 
     return(
--- customer/src/views/IndexSections/Tabs.js
+++ customer/src/views/IndexSections/Tabs.js
@@ -1,12 +1,17 @@
-
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import classnames from 'classnames';
 import GridViewProduct from './GridViewProducts';
 import { Pagination } from 'semantic-ui-react';
+import { Dropdown, Menu } from 'semantic-ui-react'
 import Search from './Search';
+
 // reactstrap components
 import {
 	TabContent,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem,
+	UncontrolledDropdown,
 	TabPane,
 	Row,
 	Card,
@@ -19,97 +24,123 @@ import {
 import GridViewService from './GridViewService';
 
 const Tabs = () => {
-	const [search, setSearch] = useState(null);
+	const [ search, setSearch ] = useState(null);
 	const updateSearch = (keyword) => {
 		setSearch(keyword);
-	}
-	const [iconTabs, seticonTabs] = useState(1);
-	const [textTabs, settextTabs] = useState(4);
+	};
+	const [category, setCategory] = useState(null);
+	const updateCategory = (keyword) => {
+		setCategory(null);
+		setCategory(keyword);
+	};
+	const [ iconTabs, seticonTabs ] = useState(1);
+	const [ textTabs, settextTabs ] = useState(4);
 	const toggleTabs = (e, stateName, index) => {
 		e.preventDefault();
-		if (stateName === 'iconTabs')
-			seticonTabs(index);
+		if (stateName === 'iconTabs') seticonTabs(index);
 		else settextTabs(index);
 	};
 
-		return (
-			<div className="section section-tabs" style={{ margin: '9%'}}>
-				<Card>
-					<CardHeader>
-						<Nav className="nav-tabs-info" role="tablist" tabs>
-							<NavItem>
-								<NavLink
-									className={classnames({
-										active: iconTabs === 1
-									})}
-									onClick={(e) => toggleTabs(e, "iconTabs", 1)}
+	const handleFilter = (e, key) => {
+		e.preventDefault();
+		setSearch(null);
+		setSearch(key);
+	}
+
+	return (
+		<div className="section section-tabs" style={{ margin: '9%' }}>
+			<Card>
+				<CardHeader>
+					<Nav className="nav-tabs-info" role="tablist" tabs>
+						<NavItem>
+							<NavLink
+								className={classnames({
+									active: iconTabs === 1
+								})}
+								onClick={(e) => toggleTabs(e, 'iconTabs', 1)}
+								href="#pablo"
+							>
+								<i className="tim-icons icon-spaceship" />
+								Products
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink
+								className={classnames({
+									active: iconTabs === 2
+								})}
+								onClick={(e) => toggleTabs(e, 'iconTabs', 2)}
+								href="#pablo"
+							>
+								<i className="tim-icons icon-settings-gear-63" />
+								Services
+							</NavLink>
+						</NavItem>
+						{iconTabs === 1 && <NavItem>
+							<UncontrolledDropdown nav>
+								<DropdownToggle
+									caret
+									color="default"
+									data-toggle="dropdown"
 									href="#pablo"
-								>
-									<i className="tim-icons icon-spaceship" />
-									Products
-								</NavLink>
-							</NavItem>
-							<NavItem>
-								<NavLink
-									className={classnames({
-										active: iconTabs === 2
-									})}
-									onClick={(e) => toggleTabs(e, "iconTabs", 2)}
-									href="#pablo"
-								>
-									<i className="tim-icons icon-settings-gear-63" />
-									Services
-								</NavLink>
-							</NavItem>
-							<NavItem>
-								<NavLink
-									className={classnames({
-										active: iconTabs === 3
-									})}
-									onClick={(e) => toggleTabs(e, "iconTabs", 3)}
-									href="#pablo"
+									nav
+									onClick={(e) => e.preventDefault()}
 								>
 									<i className="tim-icons icon-bag-16" />
-									Options
-								</NavLink>
-							</NavItem>
-                            <Search updateSearch={updateSearch}/>
-						</Nav>  
-					</CardHeader>
-					<CardBody>
-						<TabContent className="tab-space" activeTab={"link" + iconTabs}>
-							<TabPane tabId="link1">
-								<GridViewProduct search={search}/>
-							</TabPane>
-							<TabPane tabId="link2">
-								<GridViewService />
-							</TabPane>
-							<TabPane tabId="link3">
-								<p>
-									Efficiently unleash cross-media information without cross-media value. Quickly
-									maximize timely deliverables for real-time schemas. <br />
-									<br />
-									Dramatically maintain clicks-and-mortar solutions without functional solutions.
-								</p>
-							</TabPane>
-						</TabContent>
-						<Row >
-                            <Pagination
-                                style={{margin: "auto"}}
-                                boundaryRange={0}
-                                defaultActivePage={1}
-                                ellipsisItem={null}
-                                firstItem={null}
-                                lastItem={null}
-                                siblingRange={1}
-                                totalPages={10}
-                            />
-                        </Row>
-					</CardBody>
-				</Card>
-			</div>
-		);
-
-}
+									Categories
+								</DropdownToggle>
+								<DropdownMenu className="dropdown-with-icons">
+									<DropdownItem onClick={e => handleFilter(e, null)}>
+										<i className="tim-icons icon-atom" />
+										Tất cả
+									</DropdownItem>
+									<DropdownItem onClick={e => handleFilter(e, 1)}>
+										<i className="tim-icons icon-atom" />
+										Dầu nhớt
+									</DropdownItem>
+									<DropdownItem onClick={e => handleFilter(e, 2)}>
+										<i className="tim-icons icon-settings-gear-63" />
+										Phụ trợ động cơ
+									</DropdownItem>
+								</DropdownMenu>
+							</UncontrolledDropdown>
+						</NavItem>}
+						<Search updateSearch={updateSearch} />
+					</Nav>
+				</CardHeader>
+				<CardBody>
+					<TabContent className="tab-space" activeTab={'link' + iconTabs}>
+						<TabPane tabId="link1">
+							<GridViewProduct search={search} category={category}/>
+						</TabPane>
+						<TabPane tabId="link2">
+							<GridViewService />
+						</TabPane>
+						<TabPane tabId="link3">
+							<p>
+								Efficiently unleash cross-media information without cross-media value. Quickly maximize
+								timely deliverables for real-time schemas. <br />
+								<br />
+								Dramatically maintain clicks-and-mortar solutions without functional solutions.
+							</p>
+						</TabPane>
+					</TabContent>
+					<Row>
+						<Pagination
+							style={{ margin: 'auto' }}
+							boundaryRange={0}
+							defaultActivePage={1}
+							ellipsisItem={null}
+							firstItem={null}
+							lastItem={null}
+							siblingRange={1}
+							totalPages={10}
+						/>
+					</Row>
+				</CardBody>
+			</Card>
+		</div>
+	);
+};
 
 export default Tabs;
